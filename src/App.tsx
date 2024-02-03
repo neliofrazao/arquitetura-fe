7e8a4e9833b7b7ebf3eb2b9d241e204fb9d075b5
Nelio Frazao <neliofrazac@gmail.com>
2024-02-03 10:59:45 -0300

    feat: hooks de efeito

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,73 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { CounterClick, MyFirstComponent, MySecondComponent } from './components';
 
+interface Address {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  ibge: string;
+  gia: string;
+  ddd: string;
+  siafi: string;
+}
+
+const intialAddress: Address = {
+  cep: '',
+  logradouro: '',
+  complemento: '',
+  bairro: '',
+  localidade: '',
+  uf: '',
+  ibge: '',
+  gia: '',
+  ddd: '',
+  siafi: '',
+};
+
 function App() {
+  const [address, setAddress] = useState<Address>(intialAddress);
+  const [cepValue, setCepValue] = useState('58050060');
+  const [isMount, setIsMount] = useState(false);
+  // mounting
+  // updating
+  const abort = new AbortController();
+  useEffect(() => {
+    setIsMount(true);
+    fetch(`https://viacep.com.br/ws/${cepValue}/jsson/`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setAddress(data))
+      .catch((err) => alert('Cep inválido'));
+    // unmounting
+    return () => {
+      abort.abort();
+    };
+  }, [cepValue]);
+
+  console.log('endereço', address);
+
+  const handleCepChange = (value: string) => setCepValue(value);
+  console.log('cepValue', cepValue);
+
+  if (!isMount) return <p>Carregando...</p>;
   return (
     // react Fragment
     <>
+      <button onClick={() => handleCepChange('58036442')}>Buscar CEP</button>
       <CounterClick />
       <MyFirstComponent
         inputName='nome'
         labelText='Nome'
         fieldsetTitle='Input de Nome'
         placeholderText='Digite o nome corretamente'
-        value={true}
+        value={address.bairro}
       />
       <MyFirstComponent inputName='email' labelText='Email' value='email@email.com' />
       <MySecondComponent />
--- src/components/my-first-component/myFirstComponent.tsx
+++ src/components/my-first-component/myFirstComponent.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FormComponentEnum, FormComponentProps } from '../types';
 
 interface MyFirstComponentProps extends FormComponentProps {
