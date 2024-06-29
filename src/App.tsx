f2446aa13204032d669bcc2a42819bec57b78822
Nelio Frazao <neliofrazac@gmail.com>
2024-06-29 15:11:45 -0300

    criando exemplos

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import { useState } from 'react';
 import './App.css'
 import { Card } from './components';
 import { Wrapper } from './shared'
+import { Button, Container, ListGroup } from 'react-bootstrap';
 
 interface Cards {
   title?: string,
   text: string,
   complement?: string
 }
+
+
+
+
+
+// listar essas tarefas com ListGroup do react-bootstrap
+// botao de task concluida
 
 
 const cardMock:Cards[]  = [
@@ -26,21 +34,60 @@ const cardMock:Cards[]  = [
   }
 ]
 
+interface Task {
+  task: string,
+  finished: boolean
+  id: number
+
+}
+
+const listaDeAfazeres = [
+  {
+    id: 1,
+    task: 'Lista de afazeres',
+    finished: false
+  },  
+  {
+    id: 2,
+    task: 'Lista de afazeres2',
+    finished: false
+  }
+]
 
 
 function App() {
   const [showCards, setShowCards] = useState<boolean>(true)
+  const [tasks, setTasks] = useState<Task[]>(listaDeAfazeres);
 
   const onClickShowCards = () => {
     setShowCards(!showCards)
   }
 
-  console.log(showCards)
+  const toogleFinished = (index: number) => {
+    console.log('index',index)
+    const newTasks= [...tasks];	
+    console.log('newTasks',newTasks[index])
+    newTasks[index].finished = !newTasks[index].finished;
+    // console.log('clone',newTasks)
+    setTasks(newTasks);
+  }
+
+
   return (
     // fragmento do react, React.Fragment
     // <> =  Shortcut  do React.Fragment
     <>
-    <button onClick={onClickShowCards}>{showCards ? 'Esconder' : 'Mostrar'} Caixas</button>
+    <Container>
+      <ListGroup as="ul">
+        {listaDeAfazeres.map((list, index) => (
+           <ListGroup.Item as="li" key={list.id}>
+            {list.task} <Button onClick={() => toogleFinished(index)}>Concluir || Concluido</Button>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Container>
+
+    <Button onClick={onClickShowCards}>{showCards ? 'Esconder' : 'Mostrar'} Caixas</Button>
       <Wrapper title='Disciplina'>
         
         <div className={showCards ? 'show' : 'hide'}>
